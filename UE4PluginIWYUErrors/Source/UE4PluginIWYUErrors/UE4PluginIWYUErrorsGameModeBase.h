d0ad2eef129ab719d6179977e9909f4ee263dcc0
Doug Richardson <doug@rekt.email>
2019-04-29 19:17:37 -0700

    setup IWYU project

---
+++ UE4PluginIWYUErrors/Source/UE4PluginIWYUErrors/UE4PluginIWYUErrorsGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4PluginIWYUErrorsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4PLUGINIWYUERRORS_API AUE4PluginIWYUErrorsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
